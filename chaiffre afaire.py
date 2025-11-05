9e21816d671f2fb14dfe5c683f01a78170a3a2ba
imad <imadtalbioui1@gmail.com>
2025-11-05 13:50:18 +0100

    first commit

---
+++ chaiffre afaire.py
@@ -0,0 +1,5 @@
+print ("Programme qui calcul le chaifre d'affaire\n")
+quantité= int (input ("Veuillez entrer la qualité de produits vendus:")) 
+prix= float (input ("Veuillez entrer le prix de vente :"))
+ch = quantité* prix 
+print ("Le chaifre d'affaire de l'entreprise est:",ch,"DH")
---
+++ surface.py
@@ -0,0 +1,4 @@
+print("Veuillez entrer la valeur de rayon de cercle: ")
+rayon = float(input("Veuillez entrer la valeur de rayon de cercle : "))
+surface= rayon * rayon * 3.14
+print ("La surface de cercle est : ", surface)
